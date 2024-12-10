9f5b13315bbe57887254ba30e790015f77ea7428
Priyos <156951756+Priyosmita@users.noreply.github.com>
2024-12-10 13:31:02 +0530

    Create sakura.py

---
+++ sakura.py
@@ -0,0 +1,17 @@
+import turtle
+t=turtle.Turtle()
+t.shape('turtle')
+t.screen.bgcolor("black")
+t.pensize(2)
+t.color("#F4C2C2")
+t.left(90)
+t.speed(50)
+def sakura(i):
+    for i in range (300):
+        t.circle(190-i,90)
+        t.left(90)
+        t.circle(190-i,90)
+        t.left(18)
+sakura(0)
+#turtle.write("Flower")
+turtle.done()
